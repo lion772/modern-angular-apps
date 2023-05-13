5d5767d942612692f14ec3fca1ca816477dc4a19
lion772 <williamlopes64@gmail.com>
2023-05-13 09:57:01 +0200

    make app divider, as well as shared module in collection available

--- module-system/src/app/shared/shared.module.ts
+++ module-system/src/app/shared/shared.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { SharedRoutingModule } from './shared-routing.module';
 import { DividerComponent } from './divider/divider.component';
+import { TableComponent } from './table/table.component';
 
 @NgModule({
-  declarations: [DividerComponent],
+  declarations: [DividerComponent, TableComponent],
   imports: [CommonModule, SharedRoutingModule],
-  exports: [DividerComponent],
+  exports: [DividerComponent, TableComponent],
 })
 export class SharedModule {}

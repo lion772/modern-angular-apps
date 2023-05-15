fd71391e10802a5ea3adc1299396e157d9434d0e
lion772 <williamlopes64@gmail.com>
2023-05-15 12:43:51 +0200

    generate statistics component inside of view home component

--- module-system/src/app/views/views.module.ts
+++ module-system/src/app/views/views.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { ViewsRoutingModule } from './views-routing.module';
 import { ViewsHomeComponent } from './views-home/views-home.component';
+import { StatisticsComponent } from './statistics/statistics.component';
 
 @NgModule({
-  declarations: [ViewsHomeComponent],
+  declarations: [ViewsHomeComponent, StatisticsComponent],
   imports: [CommonModule, ViewsRoutingModule],
   exports: [],
 })

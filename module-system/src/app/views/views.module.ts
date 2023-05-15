e87135e7bf4cdea357647cfd0e9af218da01089a
lion772 <williamlopes64@gmail.com>
2023-05-15 19:47:21 +0200

    add divider to views home, import SharedModules into views module

--- module-system/src/app/views/views.module.ts
+++ module-system/src/app/views/views.module.ts
@@ -6,10 +6,11 @@ import { ViewsHomeComponent } from './views-home/views-home.component';
 import { StatisticsComponent } from './statistics/statistics.component';
 import { ManageViewsService } from './manage-views.service';
 import { HttpClientModule } from '@angular/common/http';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [ViewsHomeComponent, StatisticsComponent],
-  imports: [CommonModule, ViewsRoutingModule, HttpClientModule],
+  imports: [CommonModule, ViewsRoutingModule, HttpClientModule, SharedModule],
   exports: [],
   providers: [ManageViewsService],
 })

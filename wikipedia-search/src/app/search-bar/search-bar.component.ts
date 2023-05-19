70815b75d6720339f3b32bef22c999c421b18205
lion772 <williamlopes64@gmail.com>
2023-05-19 13:34:21 +0200

    install bootstrap, add onInput and OnSubmitForm methods in search-bar

--- wikipedia-search/src/app/search-bar/search-bar.component.ts
+++ wikipedia-search/src/app/search-bar/search-bar.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-search-bar',
   templateUrl: './search-bar.component.html',
-  styleUrls: ['./search-bar.component.scss']
+  styleUrls: ['./search-bar.component.scss'],
 })
 export class SearchBarComponent {
+  public term = '';
 
+  public onInput(e: any) {
+    if (e.target.value) {
+      this.term = e.target.value;
+    }
+  }
+
+  public onSubmitForm(e: any) {
+    e.preventDefault();
+    console.log(this.term);
+  }
 }

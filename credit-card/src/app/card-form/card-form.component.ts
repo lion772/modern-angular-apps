009ac5605ec1ce617709d9edf81df8ae4f0ae838
lion772 <williamlopes64@gmail.com>
2023-05-21 12:43:51 +0200

    implement reactive forms module in progress

--- credit-card/src/app/card-form/card-form.component.ts
+++ credit-card/src/app/card-form/card-form.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-card-form',
   templateUrl: './card-form.component.html',
-  styleUrls: ['./card-form.component.scss']
+  styleUrls: ['./card-form.component.scss'],
 })
-export class CardFormComponent {
+export class CardFormComponent implements OnInit {
+  public cardForm: FormGroup = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    cardNumber: new FormControl('', [Validators.required, Validators.max(8)]),
+    expiration: new FormControl('', [Validators.required]),
+    securityCode: new FormControl('', [
+      Validators.required,
+      Validators.maxLength(3),
+    ]),
+  });
 
+  public context = {
+    name: this.cardForm.get('name'),
+    cardNumber: this.cardForm.get('cardNumber'),
+    expiration: this.cardForm.get('expiration'),
+    securityCode: this.cardForm.get('securityCode'),
+  };
+
+  public ngOnInit(): void {}
+
+  public onSubmit() {
+    if (this.cardForm.valid) {
+    }
+  }
 }

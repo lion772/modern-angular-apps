63c0175dd04358cf2af72bfd989a92f32404d1f9
lion772 <williamlopes64@gmail.com>
2023-05-24 11:37:24 +0200

    add input masking functionality

---
+++ credit-card/src/app/date-form-control.ts
@@ -0,0 +1,43 @@
+import { FormControl } from '@angular/forms';
+
+export class DateFormControl extends FormControl {
+  override setValue(value: string, options: any) {
+    if (value.length > 5) {
+      super.setValue(this.value, {
+        ...options,
+        emitModelToViewChange: true,
+      });
+      return;
+    }
+
+    if (value.match(/[^0-9|\/]/gi)) {
+      super.setValue(this.value, {
+        ...options,
+        emitModelToViewChange: true,
+      });
+      return;
+    }
+
+    if (value.length === 3 && this.value.length === 2) {
+      super.setValue(value.slice(0, 2) + '/' + value.slice(-1), {
+        ...options,
+        emitModelToViewChange: true,
+      });
+      return;
+    }
+
+    if (value.length === 2 && this.value.length === 3) {
+      super.setValue(value, { ...options, emitModelToViewChange: true });
+      return;
+    }
+
+    if (value.length === 2) {
+      super.setValue(value + '/', {
+        ...options,
+        emitModelToViewChange: true,
+      });
+      return;
+    }
+    super.setValue(value, { ...options, emitModelToViewChange: true });
+  }
+}

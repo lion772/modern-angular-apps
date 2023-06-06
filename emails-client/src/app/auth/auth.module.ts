b5f96d3905d8e7c16754501b65dd9e922a26c207
lion772 <williamlopes64@gmail.com>
2023-06-06 07:43:02 +0200

    setup email-client navigation

---
+++ emails-client/src/app/auth/auth.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AuthRoutingModule } from './auth-routing.module';
+import { SignupComponent } from './signup/signup.component';
+import { SigninComponent } from './signin/signin.component';
+
+@NgModule({
+  declarations: [SignupComponent, SigninComponent],
+  imports: [CommonModule, AuthRoutingModule],
+  exports: [],
+})
+export class AuthModule {}

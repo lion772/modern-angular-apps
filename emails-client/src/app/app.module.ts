ef418f396edbd7fb31d73ef2b4e55d83e6e7a670
lion772 <williamlopes64@gmail.com>
2023-06-06 13:32:15 +0200

    handle async validation errors

--- emails-client/src/app/app.module.ts
+++ emails-client/src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthModule } from './auth/auth.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AuthModule, AppRoutingModule],
+  imports: [BrowserModule, AuthModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- emails-client/src/app/auth/signup/signup.component.ts
+++ emails-client/src/app/auth/signup/signup.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatchPassword } from '../validators/match-password';
+import { UniqueUsername } from '../validators/unique-username';
 
 @Component({
   selector: 'app-signup',
@@ -10,16 +11,20 @@ import { MatchPassword } from '../validators/match-password';
 export class SignupComponent implements OnInit {
   public signupForm = new FormGroup(
     {
-      username: new FormControl('', [
-        Validators.required,
-        Validators.minLength(3),
-        Validators.maxLength(20),
-      ]),
+      username: new FormControl(
+        '',
+        [
+          Validators.required,
+          Validators.minLength(3),
+          Validators.maxLength(20),
+          Validators.pattern(/^[a-z0-9]+$/),
+        ],
+        [this.uniqueUsername.validate]
+      ),
       password: new FormControl('', [
         Validators.required,
         Validators.minLength(4),
         Validators.maxLength(20),
-        Validators.pattern(/^[a-z0-9]+$/),
       ]),
       passwordConfirmation: new FormControl('', [
         Validators.required,
@@ -30,7 +35,10 @@ export class SignupComponent implements OnInit {
     { validators: [this.matchPassword.validate] }
   );
 
-  public constructor(private matchPassword: MatchPassword) {}
+  public constructor(
+    private matchPassword: MatchPassword,
+    private uniqueUsername: UniqueUsername
+  ) {}
 
   ngOnInit(): void {}
 

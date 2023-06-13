1a462f2036911942e5e1843ed2ad51d6800a46d9
lion772 <williamlopes64@gmail.com>
2023-06-13 15:45:39 +0200

    wire up email form group in email form, provide a form default value

--- emails-client/src/app/inbox/email-create/email-create.component.ts
+++ emails-client/src/app/inbox/email-create/email-create.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Email } from '../email';
 
 @Component({
   selector: 'app-email-create',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./email-create.component.scss'],
 })
 export class EmailCreateComponent {
-  showModal = false;
+  public showModal = false;
+  public email!: Email;
+  public constructor() {
+    this.email = {
+      id: '',
+      to: '',
+      subject: '',
+      html: '',
+      text: '',
+      from: 'test@gmail.com',
+    };
+  }
 }
--- emails-client/src/app/inbox/email-form/email-form.component.ts
+++ emails-client/src/app/inbox/email-form/email-form.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Email } from '../email';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-email-form',
   templateUrl: './email-form.component.html',
-  styleUrls: ['./email-form.component.scss']
+  styleUrls: ['./email-form.component.scss'],
 })
-export class EmailFormComponent {
+export class EmailFormComponent implements OnInit {
+  @Input() email!: Email;
+  public emailForm!: FormGroup;
 
+  ngOnInit(): void {
+    const { to, subject, html, text, from } = this.email;
+    this.emailForm = new FormGroup({
+      to: new FormControl(to, [Validators.required]),
+      subject: new FormControl(subject, [Validators.required]),
+      html: new FormControl(html, [Validators.required]),
+      text: new FormControl(text, [Validators.required]),
+      from: new FormControl(from, [Validators.required]),
+    });
+  }
 }

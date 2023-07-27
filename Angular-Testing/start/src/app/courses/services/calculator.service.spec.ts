71aab26a363997cfc7f65cad92a2b104a49005a7
lion772 <williamlopes64@gmail.com>
2023-07-27 09:32:39 +0200

    test setup using beforeEach

--- Angular-Testing/start/src/app/courses/services/calculator.service.spec.ts
+++ Angular-Testing/start/src/app/courses/services/calculator.service.spec.ts
@@ -1,22 +1,26 @@
 import { CalculatorService } from "./calculator.service";
-import { LoggerService } from "./logger.service";
 
 describe("CalculatorService", () => {
+  let calculator: CalculatorService, loggerSpy: any;
+
+  beforeEach(() => {
+    loggerSpy = jasmine.createSpyObj("LoggerService", ["log"]);
+
+    calculator = new CalculatorService(loggerSpy);
+  });
+
   it("should add two numbers", () => {
-    const loggerService = jasmine.createSpyObj("LoggerService", ["log"]);
-
-    const calculator = new CalculatorService(loggerService);
-
     const result = calculator.add(2, 2);
 
-    expect(loggerService.log).toHaveBeenCalledTimes(1);
+    expect(loggerSpy.log).toHaveBeenCalledTimes(1);
+
     expect(result).toBe(4);
   });
 
   it("should subtract two numbers", () => {
-    const calculator = new CalculatorService(new LoggerService());
-
     const result = calculator.subtract(4, 2);
+
+    expect(loggerSpy.log).toHaveBeenCalledTimes(1);
     //The second passed argument is a fail output message
     expect(result).toBe(2, "Unexpected substraction result.");
   });

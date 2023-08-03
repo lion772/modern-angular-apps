6c7bfc52ae6aedc0b503fb2e2b9d9aee73650edc
lion772 <williamlopes64@gmail.com>
2023-08-03 21:00:12 +0200

    finish local template querying with ViewChildren

--- local-template-querying/src/app/components/course-detail/course-detail.component.ts
+++ local-template-querying/src/app/components/course-detail/course-detail.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, Input } from '@angular/core';
+import { Course } from 'src/app/app.component';
 
 @Component({
   selector: 'app-course-detail',
   templateUrl: './course-detail.component.html',
-  styleUrls: ['./course-detail.component.css']
+  styleUrls: ['./course-detail.component.css'],
 })
 export class CourseDetailComponent {
-
+  @Input()
+  course!: Course;
 }

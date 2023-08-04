53f6c637987a719789f69954674639126a734b45
lion772 <williamlopes64@gmail.com>
2023-08-04 10:40:13 +0200

    generate course image component to be passed into course detail component via ng-content, use ContentChild and ContentChildren to get hold of passed element references via content projection

--- local-template-querying/src/app/app.module.ts
+++ local-template-querying/src/app/app.module.ts
@@ -6,15 +6,18 @@ import { MatCardModule } from '@angular/material/card';
 import { AppComponent } from './app.component';
 import { CourseDetailComponent } from './components/course-detail/course-detail.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { CourseImageComponent } from './components/course-image/course-image.component';
 
 @NgModule({
-  declarations: [AppComponent, CourseDetailComponent],
+  declarations: [AppComponent, CourseDetailComponent, CourseImageComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatToolbarModule,
     MatButtonModule,
     MatCardModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent],

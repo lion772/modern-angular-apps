627a0a505b623d3841cc49013b2fde75d6fda076
William <william.steinkedemello@gmail.com>
2024-03-02 22:45:42 +0100

    install @ngrx/store to manage state, create simple.reducer to display a message in app.component

--- emails-client/src/app/app.component.ts
+++ emails-client/src/app/app.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from './auth/auth.service';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, map } from 'rxjs';
+
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { AppState } from './simple.reducer';
 
 @Component({
   selector: 'app-root',
@@ -10,10 +14,14 @@ import { BehaviorSubject } from 'rxjs';
 export class AppComponent implements OnInit {
   public isUserSignedin: BehaviorSubject<boolean | null> =
     this.authService.userSignedin$;
+  
+  public message$!: Observable<String>;
+  
 
-  public constructor(private authService: AuthService) {}
+  public constructor(private authService: AuthService, private store: Store<AppState>) {}
 
   public ngOnInit(): void {
     this.authService.checkAuth().subscribe();
+    this.message$ = this.store.select("message");
   }
 }
--- emails-client/src/app/app.module.ts
+++ emails-client/src/app/app.module.ts
@@ -6,10 +6,13 @@ import { AppComponent } from './app.component';
 import { AuthModule } from './auth/auth.module';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { AuthHttpInterceptor } from './auth/auth-http-interceptor';
+import { StoreModule } from '@ngrx/store';
+import { simpleReducer } from './simple.reducer';
+
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AuthModule, AppRoutingModule, HttpClientModule],
+  imports: [BrowserModule, AuthModule, AppRoutingModule, HttpClientModule, StoreModule.forRoot({message: simpleReducer})],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: AuthHttpInterceptor, multi: true },
   ],

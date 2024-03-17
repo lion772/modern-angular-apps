683006eecf6c6ecbe3450c951333631d43423414
William <william.steinkedemello@gmail.com>
2024-03-17 10:56:54 +0100

    refactor auth.guard.ts

--- emails-client/src/app/auth/auth.guard.ts
+++ emails-client/src/app/auth/auth.guard.ts
@@ -1,32 +1,35 @@
 import { Injectable } from '@angular/core';
-import { CanLoad, Route, UrlSegment, Router } from '@angular/router';
+import { CanLoad, Router } from '@angular/router';
 import { Observable } from 'rxjs';
-import { take, skipWhile, map } from 'rxjs/operators';
+import { take, skipWhile, map, tap } from 'rxjs/operators';
 import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthGuard implements CanLoad {
+
   constructor(
     private authService: AuthService,
     private router: Router,
   ) {}
 
-  canLoad(
-    route: Route,
-    segments: UrlSegment[],
-  ): boolean | Observable<boolean> | Promise<boolean> {
+  canLoad() {
+    return this.checkAuth();
+  }
+
+  private checkAuth(): Observable<boolean> | boolean {
     return this.authService.getUserAuthentication$.pipe(
+      //Skip while null, otherwise when /inbox is hit and the page is reloaded, the page will be redirected to /signin as the first
+      // value will be null.
       skipWhile((val) => val === null),
-      //TypeScript non-null assertion operator: 'value!'
-      map((value) => value!),
       take(1),
-      map((authenticated) => {
+      map((value) => !!value),
+      tap((authenticated) => {
+        //perform a side-effect with tap without modifying the stream's elements.
         if (!authenticated) {
           this.router.navigateByUrl('/');
         }
-        return authenticated;
       }),
     );
   }

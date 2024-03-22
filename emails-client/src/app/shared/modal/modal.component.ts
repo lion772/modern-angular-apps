4c7b43e68e4aaf4a00d4f680c6fe74d0ec47b9a7
William <william.steinkedemello@gmail.com>
2024-03-22 09:24:37 +0100

    replace buttons with divs, add role button attribute, add tabindex='0' to make it keyboard-focusable, add keydown event listeners, implement handleKeyDown function

--- emails-client/src/app/shared/modal/modal.component.ts
+++ emails-client/src/app/shared/modal/modal.component.ts
@@ -18,11 +18,19 @@ export class ModalComponent implements AfterContentInit, OnDestroy {
   public constructor(private elementRef: ElementRef) {}
 
   ngAfterContentInit(): void {
+    console.log(this.elementRef.nativeElement)
     document.body.appendChild(this.elementRef.nativeElement);
   }
 
   onDismissClick() {
     this.dismiss.emit();
+  }
+
+  handleKeyDown(event: KeyboardEvent) {
+    if (event.key === 'Enter' || event.key === 'Space') {
+      this.onDismissClick();
+      event.preventDefault();
+    }
   }
 
   ngOnDestroy(): void {
